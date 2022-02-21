90f387deab8f31adf57d2e617fbdc1abd5f23233
MichalToth5 <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>
2022-02-21 18:58:41 +0100

    Dekadické čislo - input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bin-dec';
+  decimal:number = 0;
+  bits: boolean[] = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
+  onDecimalChange(){
+    if(this.decimal > 255){
+      alert("Cislo sa neda zobrazit 8 bitmi")
+    }else {
+      let pom = this.decimal.toString(2);
+      for (let i = 0; i < pom.length; i++) {
+        if (pom[i] == '1') {
+          this.bits[this.bits.length - 1 - (pom.length - 1 - i)] = true;
+        } else {
+          this.bits[this.bits.length - 1 - (pom.length - 1 - i)] = false;
+        }
+      }
+      console.log(pom)
+    }
+  }
 }
